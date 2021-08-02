aae94afe1359bfe70ff550e6154b64328f86efbe
Tommy Boatman <t_boatman@live.com>
2021-08-02 07:05:05 -0500

    fix spacing

--- ASMsnippets/stdentry.asm
+++ ASMsnippets/stdentry.asm
@@ -1,18 +1,18 @@
 *
 * STANDARD ENTRY
 *
-MAIN CSECT
-     STM   14,12,12(13)
-	 BASR  12,0
-	 USING *,12
-	 ST    13,SAVE+4
-	 LA    13,SAVE
+MAIN     CSECT
+         STM   14,12,12(13)
+         BASR  12,0
+         USING *,12
+         ST    13,SAVE+4
+         LA    13,SAVE
 *
 * STANDARD EXIT
 *
-     L     13,SAVE+4
-	 LM    14,12,12(13)
-	 LA    15,0
-	 BR    14
-SAVE DC    18F'0'
-     END   MAIN
+         L     13,SAVE+4
+         LM    14,12,12(13)
+         LA    15,0
+         BR    14
+SAVE     DC    18F'0'
+         END   MAIN
